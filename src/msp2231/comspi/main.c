50c0cee80db5c8f31fb2b58b5bbb7d4cf9af7d8f
jikagq <12010824+jikagq@users.noreply.github.com>
2018-03-06 12:11:35 +0100

    ajout de la reception spi avec forge

--- src/msp2231/comspi/main.c
+++ src/msp2231/comspi/main.c
@@ -1,5 +1,107 @@
+/*msp2132 spi reception*/
 #include <msp430.h>
 
+#define SPITAILLETRAME 16 //taille de la trame
+#define SPITAILLEVAL 6 //val max+1 nombre de slot d'un entier
+#define SPINOMBREDEVALEUR 4 //nombre de valueur a transmettre
+
+volatile unsigned int i;
+volatile int unsigned spirxbuffer=0;
+volatile char spirxtrame[SPITAILLETRAME];
+
+int freq=0;
+int etat=0;
+
+void delay (unsigned int ms)
+{
+    volatile unsigned int i, z;
+    i=100;
+    while (i--) {
+        z=ms;
+        while(z--);
+    }
+}
+
+void anvilspi(){//gestion des trames forge
+    char action;
+
+    action=spirxtrame[2];
+    switch(action){
+          case 'l':{
+                ledparam();
+                raztrame();
+                break;
+                }
+          default :{
+                raztrame();
+                break;
+                }
+          }
+    raztrame();
+}
+
+void ledparam(void){
+
+    etat = slot(1);
+    freq = slot(2);
+
+}
+
+int slot(int slotnumber){
+    int i;
+    int var=0;
+    int cptseparateur=0;
+    int pos=0;//0:gauche 1:droite
+    int debut=0;
+    int fin=0;
+
+    for(i=3;i<SPITAILLETRAME-1;i++){//fin de l'entete forge à 3
+        if((spirxtrame[i]==';') || (spirxtrame[i]=='\0')){//;gauche
+            if(pos==0){
+               pos=1;
+               debut=i;
+               cptseparateur++;
+            }else{//;droit
+               pos=0;
+               fin=i;
+               if(cptseparateur == slotnumber){
+                //coupe
+                   var=substringsemicolon(debut, fin);
+                   i=SPITAILLETRAME-1;//quite la boucle dès que la veleur est extraites pour pas se taper toutes la trame
+               }else{
+                   pos=1;
+                   debut=i;
+                   cptseparateur++;
+               }
+            }
+        }
+    }
+   return var;
+}
+
+int substringsemicolon(int debut,int fin){
+    char subsstr[6];
+    int a=0;
+    int b=0;
+    int var=-1;
+
+
+    b=debut;
+    while(b<fin){
+        subsstr[a]=spirxtrame[b+1];
+        b++;
+        a++;
+    }
+    var = atoi(subsstr);
+    return var;
+}
+
+void raztrame(){
+    for(spirxbuffer=0;spirxbuffer<SPITAILLETRAME;spirxbuffer++){//ini de la trame rx
+        spirxtrame[spirxbuffer]=" ";
+    }
+    spirxbuffer=0;
+}
 
 int main(void)
 {
@@ -22,16 +124,36 @@ int main(void)
   USICNT = 8;                           // init-load counter nb de bit p408
 
   _BIS_SR(LPM0_bits + GIE);             // Enter LPM0 w/ interrupt
+
+
+  if(etat==1){
+      P1OUT |= 0x01;
+      delay(freq);
+      P1OUT &= ~0x01;
+      delay(freq);
+  }
+
 }
 
 // USI interrupt service routine
 #pragma vector=USI_VECTOR
 __interrupt void universal_serial_interface(void)
 {
-    if ('a' == USISRL)
+   /** if ('a' == USISRL)
        P1OUT |= 0x01;
      else
        P1OUT &= ~0x01;
+    **/
+
+    unsigned char c;
+    c = USISRL;
+        if(c == '\0'){//fin de la trame
+            spirxtrame[spirxbuffer]=c;
+            anvilspi();
+        }else{//sinon on ajoute le caractaire à la chaine
+           spirxtrame[spirxbuffer]=c;
+           spirxbuffer++;
+        }
      USISRL = P1IN;
      USICNT = 8;
 }
